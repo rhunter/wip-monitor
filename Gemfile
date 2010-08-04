fd6667ceddc9d44a09980c42c8e3aa2a8865dc4f
Rob Hunter <rhunter@thoughtworks.com>
2010-08-04 23:01:04 +1000

    Initial commit.
    Reads inbox, unread and drafts from Gmail.

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "http://rubygems.org/"
+gem "ruby-gmail", "0.2.1"
+gem "mime", "0.1"
+
---
+++ script/amount_of_wip.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+require 'gmail'
+require 'yaml'
+
+def main
+  sources_file = File.expand_path("../../config/sources.yml", __FILE__)
+  sources = YAML.load_file(sources_file)
+
+  record_wip_for_mailboxes(sources)
+  print_wip
+end
+
+def record_wip_for_mailboxes(sources)
+  sources['Mailboxes'].each do |mailbox_config|
+    gmail = Gmail.new(mailbox_config['username'], mailbox_config['password'])
+    record_wip_count("Unread messages", gmail.inbox.count(:unread))
+    record_wip_count("Messages in inbox", gmail.inbox.count)
+    record_wip_count("Drafts", gmail.in_mailbox("Drafts").count)
+  end
+end
+
+def record_wip_count(category, count)
+  @wip ||= Hash.new(0)
+  @wip[category] += count
+end
+
+def print_wip
+  puts @wip.inspect
+end
+
+if __FILE__ == $PROGRAM_NAME
+  main
+end
