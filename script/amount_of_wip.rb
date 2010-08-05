57d625bbc1a177aebc9c52f1d83f58c45a053a9f
Rob Hunter <rhunter@thoughtworks.com>
2010-08-06 00:53:33 +1000

    Gmail 'Starred' messages are counted as WIP

--- script/amount_of_wip.rb
+++ script/amount_of_wip.rb
@@ -2,6 +2,13 @@
 require 'gmail'
 require 'yaml'
 
+class Gmail
+  def starred
+    @starred_mailbox_name ||= imap.list("", "[%]/Starred").first.name
+    in_mailbox(@starred_mailbox_name)
+  end
+end
+
 def main
   sources_file = File.expand_path("../../config/sources.yml", __FILE__)
   sources = YAML.load_file(sources_file)
@@ -15,6 +22,7 @@ def record_wip_for_mailboxes(sources)
     gmail = Gmail.new(mailbox_config['username'], mailbox_config['password'])
     record_wip_count("Unread messages", gmail.inbox.count(:unread))
     record_wip_count("Messages in inbox", gmail.inbox.count)
+    record_wip_count("Starred messages", gmail.starred.count)
     record_wip_count("Drafts", gmail.in_mailbox("Drafts").count)
   end
 end
